c1dbeb466d7136bccc7a299ae8c170f8bb8ef40e
Anissa Crawford <anissacrawford@gmail.com>
2022-05-07 10:50:15 -0500

    test button

---
+++ Icon.jsx
@@ -0,0 +1,7 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+function Icon(){
+    <Button component={Link} to='/shop' label="Shop" icon={<LinkedInIcon/>}/>
+}
+
+export default Icon;
