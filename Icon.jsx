19813e1ac58a72bb6f023ff99449306e53b0781d
Anissa Crawford <anissacrawford@gmail.com>
2022-05-07 10:58:16 -0500

    add links

--- Icon.jsx
+++
@@ -1,7 +0,0 @@
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-
-function Icon(){
-    <Button component={Link} to='/shop' label="Shop" icon={<LinkedInIcon/>}/>
-}
-
-export default Icon;
